b2b3dbe080170cdb62f54f28771e1a7356ef1c44
Tenzing Sherpa <sherpatenzee09@gmail.com>
2022-10-24 13:24:23 -0400

    ex 1 complete

---
+++ functions/simple_functions.js
@@ -0,0 +1,19 @@
+function favThings( myName, favMovie , favBand )
+{
+  let message = "Hello, My name is " + myName + " and my favorite movie is " + favMovie + " and I love to listen to " + favBand;
+  console.log(message);
+}
+
+ favThings( "Tenzing", "Batman" , "Beatles");
+
+ function whyImHere(lc){
+  let messages = "I am here to " + lc ;
+  console.log(messages);
+ }
+ whyImHere("learn Javascript")
+
+ function favotitePlace(favplace){
+let msg = "I would love to visit " + favplace + " to eat pizza." ;
+console.log(msg)
+}
+favotitePlace("italy");
